b6bec63aa15f24d65b517563336a5e2bf6d8a058
ramilabd <ramsmart@mail.ru>
2022-03-10 21:52:47 +0500

    build: configure the database

--- handler/handler/settings.py
+++ handler/handler/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'handler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'file_handler_db',
+        'USER': 'wanderer',
+        'PASSWORD': '0020',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
